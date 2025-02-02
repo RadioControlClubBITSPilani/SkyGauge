441369e7eabddf74523e69b3b07732564f2f240c
clodman84 <77118531+clodman84@users.noreply.github.com>
2025-02-02 06:00:47 +0530

    Delete Application directory

--- Application/README.md
+++
@@ -1,1 +0,0 @@
-# This is where the desktop logger application should be
--- Application/main.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-# for Linux users, change this to the appropriate COM port on windows
-ser = serial.Serial('/dev/ttyUSB0')
-ser.baudrate = 115200
-
-while True:
-    # if there is no newline, this is fucked.
-    print(ser.readline())

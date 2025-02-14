5b0a8205e37cca5513cb815650e62cd31a33c17e
rebant23 <rebantrana23@gmail.com>
2025-02-14 22:17:19 +0530

    moved serial monitor initialisation inside the get_serial() function

--- App/Core/clown_to_clown_coms.py
+++ App/Core/clown_to_clown_coms.py
@@ -10,13 +10,13 @@ import logging
 logger = logging.getLogger("Core.Communication")
 
 
-# change this to the appropriate com port on windows
-se = serial.Serial('/dev/ttyUSB0')
-se.baudrate = 115200
 
 
 def get_serial():
-        # readline only works when your data from the esp has \n in it
+# change this to the appropriate com port on windows
+    se = serial.Serial('/dev/COM5')
+    se.baudrate = 115200
+    # readline only works when your data from the esp has \n in it
     data = se.readline()
     data = float(data) % 100
     logger.info(data)

8949f137b0b7c2a5e56c738a2809c5bc3218b661
clodman84 <cshourin@gmail.com>
2025-02-16 02:13:24 +0530

    added vvm201 docs

--- ESP/main/ESP.c
+++ ESP/main/ESP.c
@@ -1,13 +1,15 @@
 #include "esp_lcd_types.h"
 #include "ulgl.h"
 #include "oled_esp_lcd.h"
+#include "vvm_interface.h"
 #include <stdint.h>
 
-uint8_t screen[1024];
+// uint8_t screen[1024];
 
 void app_main(void) {
-    esp_lcd_panel_handle_t panel = initialise_oled();
-    draw_text("Radio Control Club", 3, screen);
-    display_bitmap(panel, screen);
+    // esp_lcd_panel_handle_t panel = initialise_oled();
+    // draw_text("Radio Control Club", 3, screen);
+    // display_bitmap(panel, screen);
+    start_echo_task();
 }
 
---
+++ ESP/main/vvm_interface.h
@@ -0,0 +1,6 @@
+#ifndef VVM_INTERFACE 
+#define VVM_INTERFACE
+
+void start_echo_task();
+
+#endif
